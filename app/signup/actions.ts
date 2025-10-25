62194920a485e3fa8585dfa9b238ff93ecc84ec7
BatsCrow <twilliamsa776@gmail.com>
2025-10-25 03:51:39 -0400

    ADDED: Server Function to sign up a new user

---
+++ app/signup/actions.ts
@@ -0,0 +1,44 @@
+import { supabase } from "@/utils/supabase/client";
+
+export async function signUpAction({
+  email,
+  password,
+  first_name,
+  last_name,
+}: {
+  email: string;
+  password: string;
+  first_name: string;
+  last_name: string | null;
+}): Promise<Boolean> {
+  try {
+    // Create an account in supabase
+    const { data, error: accountCreateError } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (accountCreateError) throw accountCreateError;
+
+    if (!data.user) throw new Error("Could not access user object!");
+
+    // Create a profile row
+    const { error: profileRowError } = await supabase.from("profiles").insert({
+      user_id: data.user?.id,
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+    });
+
+    if (profileRowError) throw profileRowError;
+
+    return true;
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    } else {
+      console.error("An unknown error has occurred:", err);
+    }
+    return false;
+  }
+}

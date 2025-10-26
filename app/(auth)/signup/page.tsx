3c598dbeae14b73d2e0424866cd6f04e6f460246
BatsCrow <twilliamsa776@gmail.com>
2025-10-26 03:00:11 -0400

    Updated Signup Page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,24 +1,17 @@
 // components/SignupForm.jsx
 "use client";
 import { SignUpCard } from "@/components/SignUpCard";
-import { useUser } from "@/contexts/user-provider";
-
-// <-- REQUIRED DIRECTIVE FOR CLIENT COMPONENTS
 
 export default function SignupForm() {
-  const { user } = useUser();
-
   return (
     <>
-      <div className="max-w w-full min-h-screen space-y-8 p-8 bg-white rounded-xl shadow-lg">
-        <button
-          onClick={() => {
-            console.log("Current user:", user);
-          }}
-        >
-          Test User
-        </button>
-        <SignUpCard />
+      <div
+        className="flex flex-col items-center justify-center min-h-screen p-4
+      bg-gray-50 dark:bg-gray-900 sm:bg-linear-to-br sm:from-green-50 sm:to-blue-50 dark:sm:from-gray-800"
+      >
+        <div className="w-full max-w-sm">
+          <SignUpCard />
+        </div>
       </div>
     </>
   );

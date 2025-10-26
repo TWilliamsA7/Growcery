d8c70d35a3abb5585c277dd8c662cc6388d16c6d
BatsCrow <twilliamsa776@gmail.com>
2025-10-25 20:16:38 -0400

    Added: button for expanding table on home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,10 +8,12 @@ import { ProduceTable } from "@/components/ProduceTable";
 import { SelectRoleModal } from "@/components/SelectRoleModal";
 import { useProfile } from "@/contexts/profile-provider";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function HomePage() {
   const { profile } = useProfile();
   const router = useRouter();
+  const [displayLimit, setDisplayLimit] = useState<number | undefined>(5);
 
   return (
     <div>
@@ -32,14 +34,26 @@ export default function HomePage() {
         {profile?.user_type === "consumer" ? (
           <>
             <ProduceTable expired className="my-1" />
-            <ProduceTable limit={5} className="my-1" />
+            <ProduceTable limit={displayLimit} className="my-1" />
           </>
         ) : (
           <>
             <CropTable past_harvest className="my-1" />
-            <CropTable limit={5} className="my-1" />
+            <CropTable limit={displayLimit} className="my-1" />
           </>
         )}
+        <MobileButton
+          className="mb-3"
+          onClick={() => {
+            if (displayLimit) {
+              setDisplayLimit(undefined);
+            } else {
+              setDisplayLimit(5);
+            }
+          }}
+        >
+          {displayLimit ? "See More" : "See Less"}
+        </MobileButton>
       </main>
       <footer>
         <LogOutButton />

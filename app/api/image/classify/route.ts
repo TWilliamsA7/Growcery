8c8899018f223e604992c3cd352455c013e98f87
BatsCrow <twilliamsa776@gmail.com>
2025-10-26 04:39:24 -0400

    Connecting to AMD Predictor

--- app/api/image/classify/route.ts
+++ app/api/image/classify/route.ts
@@ -9,17 +9,22 @@ export async function POST(request: Request) {
 
     // Extract image from formData
     const file: File | null = data.get("image") as unknown as File;
+    const custType: string | null = data.get("type") as string | null;
 
-    if (!file) {
+    if (!file || !custType) {
+      // Check for both file and status
       return NextResponse.json(
-        { success: false, message: "No file uploaded" },
+        { success: false, message: "Missing file or type parameter" },
         { status: 400 }
       );
     }
 
+    const status = custType === "consumer" ? "produce" : "crop";
+
     // Prepare the new FormData object for the external endpoint
     const forwardFormData = new FormData();
     forwardFormData.append("file", file, file.name);
+    forwardFormData.append("status", status);
 
     // 4. Send the POST request to the external endpoint
     const response = await fetch(AMD_CLASSIFIER_ENDPOINT, {
